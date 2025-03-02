1a6c197839ec6abaeb909a7554bb18a8ed273b6a
jproney <jamesproney@gmail.com>
2025-03-02 08:13:46 -0500

    scrap hard negs for now

--- data.py
+++ data.py
@@ -28,7 +28,7 @@ def corrupt_with_ctf(batch_ptcls, batch_ctf_params, snr1, snr2, freqs, b_factor=
 
 class ContrastiveProjectionDataset(Dataset):
     def __init__(self, images, phis, thetas, object_ids, pos_angle_threshold=30, pclean=0.3, snr1=[1.5], 
-                 dfu=[10000], Apix=5.0, ang=0.0, kv=300, cs=2.7, wgh=0.1, ps=0.0, proj_per_obj=192, img_size=128, p_hard=0.25):
+                 dfu=[10000], Apix=5.0, ang=0.0, kv=300, cs=2.7, wgh=0.1, ps=0.0, proj_per_obj=192, img_size=128, p_hard=0.0):
         """
         Dataset for contrastive learning of image projections.
         
