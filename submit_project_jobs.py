4846d107ff30c2330ac33360916a0c4367b85f4b
James Roney <jamesproney@gmail.com>
2025-03-26 14:24:32 -0400

    3d search at the moment seems considerably worse than 2d. wrapping up development for now to focus on cleanup + presentation

--- submit_project_jobs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-nmaps = len(open("../siren_maps.csv").readlines())
-bsize = 10
-
-for i in range(0, nmaps, bsize):
-
-    print(f"sbatch project.sbatch {i} {i+bsize}")
-    os.system(f"sbatch project.sbatch {i} {i+bsize}")
