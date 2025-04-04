b66e5e1dbf420eed4e2d17158c35cb20a486b8d5
jproney <jamesproney@gmail.com>
2025-04-04 17:12:37 -0400

    tweak project

--- data_scripts/batch_project.py
+++ data_scripts/batch_project.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import subprocess
 import pandas as pd
+from cryofinder.data_scripts import proj
 
 parser = argparse.ArgumentParser(description='Batch process .map files with project3d.py')
 parser.add_argument('input_file', default="/home/gridsan/jroney/siren_maps.csv", help='csv with the names of volumes and metadata')
@@ -39,9 +40,10 @@ for map_name, apix, dim in zip(maplist, apixlist, sizelist):
         continue
     
     # Build command
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     cmd = [
         'python', 
-        'project3d.py',
+        os.path.join(script_dir, 'project3d.py'),
         input_path,
         outstack,
         '--apix',
